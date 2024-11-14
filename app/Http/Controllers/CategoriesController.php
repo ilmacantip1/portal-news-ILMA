1c4587170598c4b1f72e9210a1413f88bb6dd212
ilmacantip1 <desswintailma12@gmail.com>
2024-11-14 21:01:57 +0700

    Commit Kedua

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Categories;
+
+class CategoriesController extends Controller
+{
+    public function create ()
+    {
+        return view('category.tambah');
+    }
+
+    public function store (Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+        ]);
+
+        DB::table('categories')->insert([
+            'name' => $request->input('name'),
+        ]);
+
+        return redirect('/categories')->with('success', 'Data berhasil di tambahkan');
+    }
+
+    public function index ()
+    {
+        $categories = DB::table('categories')->get();
+
+        return view('category.tampil', ['categories' => $categories]);
+    }
+
+    public function edit ($id )
+    {
+        $category = DB::table('categories')->where('id', $id)->first();
+
+        return view('category.edit', ['category' => $category]);
+    }
+
+    public function update (Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+        ]);
+
+        DB::table('categories')
+            ->where('id', $id)
+            ->update(
+                [
+                    'name' => $request->input('name'),
+                ]
+            );
+
+        return redirect('/categories')->with('success', 'Data berhasil di edit');
+    }
+
+
+    public function destroy ($id)
+    {
+        DB::table('categories')->where('id', $id)->delete();
+
+        return redirect('/categories')->with('success', 'Data berhasil di delete');
+    }
+
+    public function show($id)
+    {
+        $categories = Categories::find($id);
+
+        return view('category.detail', ['categories' => $categories]);
+    }
+}
